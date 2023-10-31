e783289f92b30f064c2a286a704b0f432c3ae16e
tommitah <reijotahvanainen15@gmail.com>
2023-10-31 23:56:31 +0200

    keymaps for testing plug

--- lua/config/tests/test.lua
+++ lua/config/tests/test.lua
@@ -4,6 +4,12 @@ local M = {
 
 function M.config()
   require("nvim-test").setup()
+  local opts = { noremap = true, silent = true }
+  vim.keymap.set("n", "<leader>tn", ":TestNearest<cr>", opts)
+  vim.keymap.set("n", "<leader>tf", ":TestFile<cr>", opts)
+  vim.keymap.set("n", "<leader>ts", ":TestSuite<cr>", opts)
+  vim.keymap.set("n", "<leader>tl", ":TestLast<cr>", opts)
+  vim.keymap.set("n", "<leader>te", ":TestEdit<cr>", opts)
 end
 
 return M
