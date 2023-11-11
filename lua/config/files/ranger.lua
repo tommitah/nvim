7e5fd48d80e906bd94af4ff586d6e711a4482b94
tommitah <reijotahvanainen15@gmail.com>
2023-11-11 03:09:16 +0200

    ranger

--- lua/config/files/ranger.lua
+++ lua/config/files/ranger.lua
@@ -4,7 +4,7 @@ local M = {
 
 function M.config()
     local opts = { noremap = true, silent = true }
-    vim.keymap.set("n", "-", "<cmd>RnvimrToggle<cr>", opts)
+    -- vim.keymap.set("n", "-", "<cmd>RnvimrToggle<cr>", opts)
     vim.cmd[[
        let g:rnvimr_layout = {
             \ 'relative': 'editor',
