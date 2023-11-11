ccbd864962c1cac0fbda3372301f31668d9b91e4
tommitah <reijotahvanainen15@gmail.com>
2023-11-11 02:51:58 +0200

    reenabled oil

--- lua/config/files/oil.lua
+++ lua/config/files/oil.lua
@@ -1,11 +1,11 @@
 local M = {
-  "stevearc/oil.nvim",
+    "stevearc/oil.nvim",
 }
 
 function M.config()
-  -- require("oil").setup()
-  -- local opts = { noremap = true, silent = true }
-  -- vim.keymap.set("n", "-", "<cmd>Oil<cr>", opts)
+    require("oil").setup()
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set("n", "-", "<cmd>Oil<cr>", opts)
 end
 
 return M
--- lua/config/files/ranger.lua
+++ lua/config/files/ranger.lua
@@ -5,6 +5,16 @@ local M = {
 function M.config()
     local opts = { noremap = true, silent = true }
     vim.keymap.set("n", "-", "<cmd>RnvimrToggle<cr>", opts)
+    vim.cmd[[
+       let g:rnvimr_layout = {
+            \ 'relative': 'editor',
+            \ 'width': &columns,
+            \ 'height': &lines - 2,
+            \ 'col': 0,
+            \ 'row': 0,
+            \ 'style': 'minimal'
+            \ }
+    ]]
 end
 
 return M
