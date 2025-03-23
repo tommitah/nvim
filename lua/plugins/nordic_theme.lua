f5ffe3c83f8498e3917129ac3543fa1bb7c4425f
tommitah <reijotahvanainen15@gmail.com>
2025-03-23 16:26:42 +0200

    remove all

--- lua/plugins/nordic_theme.lua
+++
@@ -1,7 +0,0 @@
-require("nordic").setup({
-  reduced_blue = true,
-  cursorline = {
-    theme = 'light'
-  },
-	transparent_bg = true,
-})
--- lua/plugins/zenbones.lua
+++
@@ -1,3 +0,0 @@
-vim.g.zenbones = { lightness = "dim", transparent_background = false }
-vim.cmd([[ set background=light ]])
-vim.cmd.colorscheme 'zenbones'
